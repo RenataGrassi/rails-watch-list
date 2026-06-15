ffd3607c572d1a8fb15974ff7373f7d3e6d611b0
Renata Grassi <renagrassi@gmail.com>
2026-06-15 18:39:11 -0300

    primeira view funcionando (index)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
-// app/assets/config/manifest.js
 //= link_tree ../images
-//= link_tree ../../javascript .js
 //= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
 //= link popper.js
 //= link bootstrap.min.js
